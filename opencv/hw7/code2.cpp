ae5835be76688f04fd125353aec5a70af64d099a
knot41 <2410580994@qq.com>
2024-02-23 21:42:19 +0800

    伽马矫正 鱼骨图

---
+++ opencv/hw7/code2.cpp
@@ -0,0 +1,24 @@
+#include<opencv2/opencv.hpp>
+using namespace cv;
+
+int main(){
+    Mat src;
+    src = imread("/home/jiang/桌面/hw/Assessment_winter/opencv/hw7/img2.jpeg",0);
+    imshow("src",src);
+    Mat gama;
+    float gamma = 2.2;
+    gama.create(src.size(),src.type());
+    int height = src.rows;
+    int width = src.cols;
+    for(int i = 0;i < height;i++){
+        for(int j = 0;j < width;j++){
+            float gray = src.at<uchar>(i,j);
+            gray = (gray + 0.5) / 256;
+            gray = pow(gray,1/gamma);
+            gama.at<uchar>(i,j) =(int)(gray * 256 -0.5);
+        }
+    }
+    imshow("dst",gama);
+    waitKey(0);
+    return 0;
+}
