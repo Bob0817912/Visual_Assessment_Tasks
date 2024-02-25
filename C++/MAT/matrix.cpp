6165f7d5ddd603f82ef156ac51fe106375acf76b
knot41 <2410580994@qq.com>
2024-02-26 00:12:29 +0800

    这是主函数

---
+++ C++/MAT/matrix.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include "matrix.h"
+
+int main() {
+    int rows, cols;
+
+    std::cin >> rows >> cols;
+    Matrix A(rows, cols);
+    std::cin >> A;
+
+    std::cin >> rows >> cols;
+    Matrix B(rows, cols);
+    std::cin >> B;
+
+    std::cout << A * B;
+
+    return 0;
+}
