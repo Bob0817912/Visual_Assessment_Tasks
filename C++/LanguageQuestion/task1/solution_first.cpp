33bbbc2c0568f7ad0013c885d3989ae697d10393
knot41 <2410580994@qq.com>
2024-02-25 13:10:52 +0800

    完成问题一

---
+++ C++/LanguageQuestion/task1/solution_first.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Person {
+    public:
+        Person(const string& personName, int personAge);
+        void display()const;
+    private:
+        string name;
+        int age;
+};
+Person :: Person(const string& personName, int personAge){
+    name = personName;
+    age = personAge;
+}
+void Person :: display()const{
+    cout << name <<" " << age << endl;
+    }
+
+int main() {
+    Person person1("ZhangSan",10),person2("LiSi",11);
+    person1.display();
+    person2.display();
+    return 0;
+}
