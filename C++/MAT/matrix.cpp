8f51cb2fc5abef2635a7479e6393ccd138c09395
hitori bocchi <115414748+Bob0817912@users.noreply.github.com>
2024-02-26 14:26:23 +0800

    Update matrix.cpp
    修改了一些可读性操作

--- C++/MAT/matrix.cpp
+++ C++/MAT/matrix.cpp
@@ -3,13 +3,16 @@
 
 int main() {
     int rows, cols;
-
-    std::cin >> rows >> cols;
+    
+    std::cin>>"请输入行和列" ;
+    std::cin>> rows >> cols;
     Matrix A(rows, cols);
+    std::cin >> "请输入矩阵A";
     std::cin >> A;
 
     std::cin >> rows >> cols;
     Matrix B(rows, cols);
+    std::cin >> "请输入矩阵B";
     std::cin >> B;
 
     std::cout << A * B;
